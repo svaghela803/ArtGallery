e35756ddf1234c1ac1fc637353d1638ee741092f
Shivangi <shivangi_vaghela@hotmail.co.uk>
2022-08-17 16:26:15 +0100

    properties for customer class

--- src/main/java/Package/Customer.java
+++ src/main/java/Package/Customer.java
@@ -1,4 +1,27 @@
 package Package;
 
 public class Customer {
+    String customerName;
+    double wallet;
+
+    public Customer(String customerName, double wallet) {
+    this.customerName = customerName;
+    this.wallet = wallet;
+    }
+
+    public String getcustomerName() {
+        return customerName;
+    }
+    public void setcustomerName(String customerName){
+    this.customerName = customerName;
+    }
+
+
+    public double getwallet() {
+    return wallet;
+    }
+    public void setwallet(double wallet) {
+    this.wallet = wallet;
+    }
+
 }
