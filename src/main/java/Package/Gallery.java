cb52d5effa20dc81eb5f9325dc09bd329a01eded
Shivangi <shivangi_vaghela@hotmail.co.uk>
2022-08-17 16:59:49 +0100

    created main and gallery properties

---
+++ src/main/java/Package/Gallery.java
@@ -0,0 +1,21 @@
+package Package;
+
+public class Gallery {
+
+    double till;
+
+    public Gallery(double till){
+        this.till = till;
+    }
+
+    public double gettill(){
+        return till;
+    }
+
+    public void settill(double till){
+        this.till = till;
+    }
+
+
+
+}
