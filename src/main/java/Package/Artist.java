6ed3c6e8174689d3ef824881fa7acec146f20467
Shivangi <shivangi_vaghela@hotmail.co.uk>
2022-08-17 16:14:30 +0100

    properties for artist class

--- src/main/java/Package/Artist.java
+++ src/main/java/Package/Artist.java
@@ -1,4 +1,18 @@
 package Package;
 
 public class Artist {
+String artistName;
+
+public Artist(String artistName){
+    this.artistName = artistName;
+}
+
+public String getArtistName() {
+return artistName;
+}
+
+public void setArtistName(String artistName) {
+this.artistName = artistName;
+}
+
 }
--- src/main/java/Package/Artwork.java
+++ src/main/java/Package/Artwork.java
@@ -5,7 +5,53 @@ public class Artwork {
 String title;
 String artist;
 double price;
+int nft;
+
+
+public String Artwork(String title, String artist, double price, int nft) {
+    this.title = title;
+    this.artist = artist;
+    this.price = price;
+    this.nft = nft;
+}
+
+    public String gettitle() {
+        return title;
+    }
+
+    public void settitle(String title){
+    this.title = title;
+    }
+
+    public String getartist() {
+    return artist;
+    }
+
+    public void setartist(String artist){
+    this.artist = artist;
+    }
+
+    public double getprice() {
+    return price;
+    }
+
+    public void setprice(double price){
+    this.price = price;
+    }
+
+    public int getnft() {
+    return nft;
+    }
+
+    public void setnft(){
+    this.nft = nft;
+    }
+
+
 
 
 
 }
+
+
+
