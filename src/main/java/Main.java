40a0ed2032610f88f6fe49145c4205738c2e86a6
Shivangi <shivangi_vaghela@hotmail.co.uk>
2022-08-17 23:18:47 +0100

    created customer method in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,15 @@
 import java.util.ArrayList;
 import Package.Artwork
+        import Package.Customer
 
 public class Main {
 
     public static void main(String[] args) {
 
         Artwork artwork = new Artwork("Mona Lisa", "Da Vinci", 100, 2));
+        Customer customer = new Customer("John", 500);
+
+        
 
 
     }
